bcac88b4952013a0d43225d7d31dfd98fcb42ec6
Jorge Morales <desarrollador1@cetoco.com>
2016-08-24 13:57:19 -0500

    Learning Testing in React

---
+++ README.md
@@ -0,0 +1,31 @@
+# React Testing
+
+Learning how to test apps in React using Mocha and Chai.
+
+## Install
+Install all the dependencies by running:
+
+```
+npm install
+```
+
+## Develop
+Run a Webpack development server with hotreload on [http://localhost:8080/].
+
+```
+npm run dev
+```
+
+## Build
+Use webpack to build the project and bundle all the application into a single file in the `dist` folder.
+
+```
+ npm run build
+```
+
+# Test
+Run the test using mocha with the following command:
+
+```
+npm run test:watch
+```
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+/* eslint-disable */
+var wepack = require('webpack');
+var path = require('path');
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+
+var BUILD_DIR = path.resolve(__dirname, 'dist');
+var APP_DIR = path.resolve(__dirname, 'src');
+
+var config = {
+  entry: APP_DIR + '/index.jsx',
+
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    root: APP_DIR,
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        include: APP_DIR,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test:  /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+      }
+    ]
+  },
+
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: APP_DIR + '/index.html'
+    })
+  ],
+
+ devServer: {
+    historyApiFallback: true,
+    contentBase: './src'
+  },
+
+  output: {
+    path: BUILD_DIR,
+    publicPath: '/',
+    filename: 'bundle.[hash].js'
+  }
+};
+
+module.exports = config;
